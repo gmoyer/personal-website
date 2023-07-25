60ecf42b333d20f0605843701e37dd877ba5d080
Griffin Moyer <gmoyer@goldensoftware.com>
2023-07-24 21:34:38 -0600

    bubble hover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { state, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 
 class Bubble {
   x : number = 0;
   y : number = 0;
+  z : number = 0;
+  mx : number = 50;
+  my : number = 50;
   size : number;
   color : number;
   hovering : boolean = false;
@@ -15,6 +18,7 @@ class Bubble {
     while (!goodSpot && attempts < 100) {
       this.x = Math.floor(Math.random() * 101);
       this.y = Math.floor(Math.random() * 101);
+      this.z = Math.random()*3+3;
       goodSpot = true;
       bubbles.forEach(bubble => {
         if (Math.abs(bubble.x - this.x) <= 4 && Math.abs(bubble.y - this.y) <= 4)
@@ -29,12 +33,19 @@ class Bubble {
 
   styles() : {[key: string]: any} {
     return {
-      left: this.x + '%',
-      top: this.y + '%',
+      left: this.x + ((0.5-this.mx)*this.z) + '%',
+      top: this.y + ((0.5-this.my)*this.z) + '%',
       backgroundColor: this.getColor(),
       width: this.size + 'px',
       height: this.size + 'px'
     }
+  }
+
+  setHovering(hovering : boolean) {
+    if (hovering)
+      this.hovering = true;
+    else
+      setTimeout(() => this.hovering = false, 2000);
   }
 
   getColor() {
@@ -51,14 +62,22 @@ class Bubble {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   bubbles : Bubble[] = [];
   bubbleCount : number = 200;
-
 
   constructor() {
     for (var i = 0; i < this.bubbleCount; i++) {
       this.bubbles.push(new Bubble(this.bubbles));
     }
+  }
+
+  ngOnInit(): void {
+    addEventListener("mousemove", (event) => {
+      this.bubbles.forEach(bubble => {
+        bubble.mx = event.clientX / window.innerWidth;
+        bubble.my = event.clientY / window.innerHeight;
+      })
+    })
   }
 }
